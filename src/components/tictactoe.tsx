2b486d2f30811dcefd7116956b9931868b9e62c3
Ellen Skipper <ellen.skipper@softwire.com>
2018-07-31 12:26:14 +0100

    TicTacToe appears on website

---
+++ src/components/tictactoe.tsx
@@ -0,0 +1,194 @@
+import * as React from 'react';
+//import './index.css';
+
+function Square(props: any) {
+  return (
+    <button className={props.isWinning} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+interface BoardProps {
+  squares: Array<any>;
+  onClick: any;
+  winningArray: any;
+}
+
+class Board extends React.Component<BoardProps, {}> {
+  renderSquare(i: number) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        isWinning={this.props.winningArray[i]}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+interface GameProps {}
+
+interface GameState {
+  history: Array<{ squares: Array<any>; clickedSquare?: Array<number> }>;
+  stepNumber: number;
+  xIsNext: boolean;
+}
+
+export class Game extends React.Component<GameProps, GameState> {
+  constructor(props: GameProps) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i: number) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+          clickedSquare: [Math.floor((i % 3) + 1), Math.floor(i / 3 + 1)],
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step: any, move: number) => {
+      const clickedSquare = step.clickedSquare;
+      const desc = move
+        ? `Go to move #${move} (Col: ${clickedSquare[0]} Row:${
+            clickedSquare[1]
+          })`
+        : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner[0];
+    } else if (current.squares.includes(null) === false) {
+      status = 'Tie: No available moves remaining';
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+            winningArray={createWinningArray(
+              calculateWinner(history[this.state.stepNumber].squares)
+            )}
+          />
+        </div>
+        Toggle list order
+        <input type="checkbox" id="toggle" className="toggle" />
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+function calculateWinner(squares: Array<any>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [squares[a], [a, b, c]];
+    }
+  }
+  return null;
+}
+
+function createWinningArray(winningSquares: Array<any>) {
+  let winningArray = [
+    'square',
+    'square',
+    'square',
+    'square',
+    'square',
+    'square',
+    'square',
+    'square',
+    'square',
+  ];
+  if (winningSquares) {
+    for (let i = 0; i < 3; i++) {
+      winningArray[parseInt(winningSquares[1][i], 10)] = 'winning square';
+    }
+  }
+  console.log(winningArray);
+  return winningArray;
+}
+
+export default Game;
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import Link from 'gatsby-link';
 
+import Game from '../components/tictactoe';
+
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <Game />
   </div>
 );
 
