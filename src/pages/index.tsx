119d264d0c983beaa51e69ab547e06ebc761377e
Philippa Hack <Philippa.Hack@softwire.com>
2018-07-31 12:14:46 +0100

    Changed any types to the proper interfaces

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
 import * as _ from 'lodash';
 
-const cellState = { DEAD: 'dead', ALIVE: 'alive' };
+enum cellState { DEAD, ALIVE }
+const cellClass = {
+  [cellState.DEAD]: 'dead',
+  [cellState.ALIVE]: 'alive',
+};
 
-const Square = (props: any) => (
-  <button className={props.value} onClick={props.onClick} >
+interface SquareProps {
+  styleClass: string;
+  onClickHandler: () => void;
+}
+
+const Square = (props: SquareProps) => (
+  <button className={props.styleClass} onClick={props.onClickHandler} >
   </button>
 );
 
-class Board extends React.Component {
+interface BoardState {
+  squares: cellState[];
+}
+
+class Board extends React.Component<{}, BoardState> {
   sideLength: number;
-  state: any;
-  constructor(props: any) {
+  constructor(props: {}) {
     super(props);
     this.timestep = this.timestep.bind(this);
     this.sideLength = 50;
@@ -30,22 +42,18 @@ class Board extends React.Component {
     return (
       <Square
         key={i}
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        styleClass={cellClass[this.state.squares[i]]}
+        onClickHandler={() => this.handleClick(i)}
       />
     );
   }
 
   timestep() {
     // Figure out how many live neighbours each cell has
-    // Any live cell with fewer than two live neighbors dies, as if by under population.
-    // Any live cell with two or three live neighbors lives on to the next generation.
-    // Any live cell with more than three live neighbors dies, as if by overpopulation.
-    // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
     console.log('step');
     const aliveNeighbourCount: number[] = [];
     this.state.squares.forEach((square: any, i: number) => {
-      // grid offsets to neighbours
+      // List of grid offsets to neighbours
       const offsets: number[][] = [[-1, -1], [0, -1], [+1, -1], [-1, 0], [+1, 0], [-1, +1], [0, +1], [+1, +1]];
       let aliveNeighbours = 0;
       offsets.forEach(pair => {
@@ -54,7 +62,7 @@ class Board extends React.Component {
         const neighbour = { x: row + dx, y: col + dy };
         if (neighbour.x < 0 || neighbour.x >= this.sideLength
           || neighbour.y < 0 || neighbour.y >= this.sideLength) {
-          // out of bounds
+          // Neighbour cell is out of bounds
           return;
         }
 
@@ -68,8 +76,12 @@ class Board extends React.Component {
     // Once we have the number of live neighbours we can modify the state
     const newState = _.zipWith(this.state.squares, aliveNeighbourCount, (square, nLiveNeighbours: number) => {
       if (square === cellState.ALIVE) {
+        // Any live cell with fewer than two live neighbors dies, as if by under population.
+        // Any live cell with two or three live neighbors lives on to the next generation.
+        // Any live cell with more than three live neighbors dies, as if by overpopulation.
         return (nLiveNeighbours < 2 || nLiveNeighbours > 3) ? cellState.DEAD : cellState.ALIVE;
       } else {
+        // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
         return (nLiveNeighbours === 3) ? cellState.ALIVE : cellState.DEAD;
       }
     });
