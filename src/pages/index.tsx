68877993bf8e6ceee391e3cb5ca5eab10cfba3af
Joanna Cheng <Joanna.Cheng@softwire.com>
2018-08-01 17:52:38 +0100

    charizard pic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,6 @@ import Link from 'gatsby-link';
 export default () => (
   <div>
     <h1>Hi people</h1>
-    <img
-      src="http://www.stickpng.com/assets/images/5859662e4f6ae202fedf2878.png"
-      height="300px"
-    />
+    <img src="/assets/charizard.png" height="300px" />
   </div>
 );
