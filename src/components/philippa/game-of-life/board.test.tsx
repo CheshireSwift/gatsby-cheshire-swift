1bd321bc38cd53bb79338d07013bda5d91e3c076
Philippa Hack <Philippa.Hack@softwire.com>
2018-08-06 16:24:48 +0100

    Renaming

--- src/components/philippa/game-of-life/board.test.tsx
+++ src/components/philippa/game-of-life/board.test.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { render, shallow, mount } from 'enzyme';
-import * as cell from './philippa/cellState';
-import { Board } from './philippa/board';
-import { Grid } from './philippa/grid';
+import * as cell from './cellState';
+import { Board } from './board';
+import { Grid } from './grid';
 import * as Square from './square';
 
 describe('Square', () => {
   it('calls onClick when clicked', () => {
     const onClick = jest.fn();
     const component = shallow(
-      <Square cellState={cell.state.DEAD} onClick={onClick} />
+      <Square cellState={cell.state.DEAD} onClick={onClick} />,
     );
     component.simulate('click');
     expect(onClick).toHaveBeenCalled();
@@ -18,7 +18,7 @@ describe('Square', () => {
   it('calls onClick when Square clicked', () => {
     const onClick = jest.fn();
     const component = shallow(
-      <Square cellState={cell.state.DEAD} onClick={onClick} />
+      <Square cellState={cell.state.DEAD} onClick={onClick} />,
     );
     component.simulate('click');
     expect(onClick).toHaveBeenCalled();
@@ -40,7 +40,7 @@ const initialState = [
 describe('Grid', () => {
   it('has as many buttons as squares', () => {
     const grid = render(
-      <Grid squares={initialState} sideLength={3} onClickHandler={() => null} />
+      <Grid squares={initialState} sideLength={3} onClickHandler={() => null} />,
     );
     // Get Grid child, then get Square child
     // Should be dead
